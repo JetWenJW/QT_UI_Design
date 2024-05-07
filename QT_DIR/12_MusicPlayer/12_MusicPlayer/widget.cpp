d2dfede88d3611a90ab77aa7002feaa497d3c839
JetWen <killer500k@gmail.com>
2024-05-07 13:37:25 +0800

    Music Player Design Project

---
+++ QT_DIR/12_MusicPlayer/12_MusicPlayer/widget.cpp
@@ -0,0 +1,37 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->pushButton->setCheckable(true);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    qDebug() << " Clicked " <<endl;
+}
+
+void Widget::on_pushButton_pressed()
+{
+    qDebug() << " Pressed~ " <<endl;
+}
+
+void Widget::on_pushButton_released()
+{
+    qDebug() << " Release~ " <<endl;
+}
+
+void Widget::on_pushButton_toggled(bool checked)
+{
+    qDebug() << " Toggled~ " <<endl;
+}
