dbc0468e7bb6f83d4b1c627ead29038aefe885ff
JetWen <killer500k@gmail.com>
2024-05-07 13:10:41 +0800

    QWidget Detail Study & Practicing Use QWidget as little parts of a window

---
+++ QT_DIR/10_QWidget_Detail/build-10_QWidget_Detail-Desktop_Qt_5_12_9_GCC_64bit-Debug/ui_widget.h
@@ -0,0 +1,60 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.9
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QWidget *widget;
+    QLabel *label;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(800, 600);
+        widget = new QWidget(Widget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(0, 0, 801, 81));
+        widget->setStyleSheet(QString::fromUtf8("QWidget\n"
+"{\n"
+"	background-color: rgb(127, 255, 0)\n"
+"}"));
+        label = new QLabel(widget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(310, 30, 241, 21));
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", nullptr));
+        label->setText(QApplication::translate("Widget", "QWdiget Text", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
