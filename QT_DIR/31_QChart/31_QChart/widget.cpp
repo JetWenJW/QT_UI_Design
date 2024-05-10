515144ae3aef8a7eab997882b8b26e1823718d70
JetWen <killer500k@gmail.com>
2024-05-10 22:37:16 +0800

    Create a Chart of Temp/Time Curve & Practicing QChart

--- QT_DIR/31_QChart/31_QChart/widget.cpp
+++ QT_DIR/31_QChart/31_QChart/widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QValueAxis>
+#include <QSplineSeries>
+#include <QLineSeries>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -25,7 +27,43 @@ Widget::Widget(QWidget *parent)
     valueAxisX->setLabelFormat("%d");
     valueAxisY->setLabelFormat("%d");
 
-    /* Step6.  */
+    valueAxisX->setTickCount(10);       /* Set Grid of X axis */
+    valueAxisY->setTickCount(10);       /* Set Grid of Y axis */
+
+
+    /* Step6. Chart add Ordinated */
+    chart->createDefaultAxes();
+    chart->addAxis(valueAxisX, Qt::AlignBottom);
+    chart->addAxis(valueAxisY, Qt::AlignLeft);
+
+    /* Step7. Set Topic & legend of Chart */
+    chart->setTitle("Temp & Time Curve");
+    chart->legend()->setVisible(true);
+
+    /* Step8. Set Curve, add all point and connect them */
+    QSplineSeries *splineSeries = new QSplineSeries();
+    splineSeries->append(0, 50);
+    splineSeries->append(1000, 60);
+    splineSeries->append(2000, 80);
+    splineSeries->append(3000, 50);
+    splineSeries->append(4000, 30);
+    splineSeries->append(5000, 80);
+
+    QPen pen(QColor(0xFF5566));
+    splineSeries->setPen(pen);
+
+    /* Step9. Add Curve to Chart */
+    chart->addSeries(splineSeries);
+
+    /* Step10. Connect all the point in Ordinate */
+    splineSeries->attachAxis(valueAxisX);
+    splineSeries->attachAxis(valueAxisY);
+
+    /* Step11. Put Chart into ChartView */
+    ui->ChartView->setChart(chart);
+
+
+
 
 }
 
